2dcd61c1a4f752280c071247938511660bf1a187
Phan Thanh Ngân <68234090+PRubyle@users.noreply.github.com>
2023-03-07 16:08:32 +0700

    Update ShipmentService.cs

--- Services/ShipmentService.cs
+++ Services/ShipmentService.cs
@@ -1,15 +1,11 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 using SWP391.Project.Models.Dtos.Address;
 
 namespace SWP391.Project.Services
 {
     public interface IShipmentService
     {
-
+        Task<AddressDto> GetShipmentAddressAsync(Guid addressId);
+        Task<ICollection<AddressDto>> GetShipmentAddressCollectionAsync(Guid accountId);
         Task<bool> AddShipmentAddressAsync(AddAddressDto input);
         Task<bool> RemoveShipmentAddressAsync(Guid addressId);
     }
@@ -17,6 +13,16 @@ namespace SWP391.Project.Services
     public class ShipmentService : IShipmentService
     {
         public Task<bool> AddShipmentAddressAsync(AddAddressDto input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<AddressDto> GetShipmentAddressAsync(Guid addressId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ICollection<AddressDto>> GetShipmentAddressCollectionAsync(Guid accountId)
         {
             throw new NotImplementedException();
         }
