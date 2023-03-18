a7ab50c2fbef8899dd727551e69687914ec371fb
Haolambor <83999494+Haolambor@users.noreply.github.com>
2023-03-18 14:59:06 +0700

    Update CartDto.cs

--- Models/Dtos/Cart/CartDto.cs
+++ Models/Dtos/Cart/CartDto.cs
@@ -1,9 +1,17 @@
+using AutoMapper.Configuration.Annotations;
+
 using SWP391.Project.Entities;
 
 namespace SWP391.Project.Models.Dtos.Cart
 {
-    public class CartDto : CartEntity
+    public class CartDto : CartSimplified
     {
-
+        // public AccountSimplified Account { get; set; } = null!;
+        [Ignore]
+        public ProductSimplified Product { get; set; } = null!;
+        [Ignore]
+        public ColorSimplified Color { get; set; } = null!;
+        [Ignore]
+        public SizeSimplified Size { get; set; } = null!;
     }
 }
