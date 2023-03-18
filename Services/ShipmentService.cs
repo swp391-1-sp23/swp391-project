9bcc84c15f555f0d8233212b8daeb9ddd951a98e
Phan Thanh Ngân <68234090+PRubyle@users.noreply.github.com>
2023-03-18 13:46:39 +0700

    Update ShipmentService.cs

--- Services/ShipmentService.cs
+++ Services/ShipmentService.cs
@@ -1,35 +1,70 @@
+using AutoMapper;
+ 
+using SWP391.Project.Entities;
 using SWP391.Project.Models.Dtos.Address;
-
+using SWP391.Project.Repositories;
+ 
 namespace SWP391.Project.Services
 {
     public interface IShipmentService
     {
         Task<AddressDto> GetShipmentAddressAsync(Guid addressId);
         Task<ICollection<AddressDto>> GetShipmentAddressCollectionAsync(Guid accountId);
-        Task<bool> AddShipmentAddressAsync(AddAddressDto input);
-        Task<bool> RemoveShipmentAddressAsync(Guid addressId);
+        Task<bool> AddShipmentAddressAsync(Guid accountId, AddAddressDto input);
+        Task<bool> RemoveShipmentAddressAsync(Guid accountId, Guid addressId);
     }
-
-    public class ShipmentService : IShipmentService
+ 
+    public class ShipmentService : BaseService, IShipmentService
     {
-        public Task<bool> AddShipmentAddressAsync(AddAddressDto input)
+        private readonly IAddressRepository _addressRepository;
+        private readonly IAccountRepository _accountRepository;
+ 
+        public ShipmentService(IAddressRepository addressRepository,
+                               IAccountRepository accountRepository,
+                               IMapper mapper) : base(mapper)
         {
-            throw new NotImplementedException();
+            _addressRepository = addressRepository;
+            _accountRepository = accountRepository;
         }
-
-        public Task<AddressDto> GetShipmentAddressAsync(Guid addressId)
+ 
+        public async Task<bool> AddShipmentAddressAsync(Guid accountId, AddAddressDto input)
         {
-            throw new NotImplementedException();
+            AddressEntity newAddress = Mapper.Map<AddressEntity>(input);
+ 
+            newAddress.Account = await _accountRepository.GetByIdAsync(entityId: accountId);
+ 
+            ICollection<AddressEntity> existingAddresses = await _addressRepository.GetCollectionAsync(predicate: address => address.Account!.Id == accountId);
+ 
+            return !existingAddresses.Any(address => address.IsPrimary && newAddress.IsPrimary)
+                   && await _addressRepository.AddAsync(entity: newAddress);
         }
-
-        public Task<ICollection<AddressDto>> GetShipmentAddressCollectionAsync(Guid accountId)
+ 
+        public async Task<AddressDto> GetShipmentAddressAsync(Guid addressId)
         {
-            throw new NotImplementedException();
+            AddressEntity? existingAddress = await _addressRepository.GetByIdAsync(entityId: addressId);
+ 
+            return Mapper.Map<AddressDto>(existingAddress);
         }
-
-        public Task<bool> RemoveShipmentAddressAsync(Guid addressId)
+ 
+        public async Task<ICollection<AddressDto>> GetShipmentAddressCollectionAsync(Guid accountId)
         {
-            throw new NotImplementedException();
+            ICollection<AddressEntity> addressCollection = await _addressRepository.GetCollectionAsync(predicate: address => address.Account!.Id == accountId);
+ 
+            List<AddressDto> addressCollectionDto = Mapper.Map<ICollection<AddressDto>>(addressCollection).Select(selector: (item, idx) =>
+            {
+                item.Account = Mapper.Map<AccountSimplified>(addressCollection.ElementAt(idx).Account);
+ 
+                return item;
+            }).ToList();
+ 
+            return addressCollectionDto;
+        }
+ 
+        public async Task<bool> RemoveShipmentAddressAsync(Guid accountId, Guid addressId)
+        {
+            AddressEntity? existingAddress = await _addressRepository.GetByIdAsync(entityId: addressId);
+ 
+            return existingAddress != null && await _addressRepository.RemoveAsync(entity: existingAddress);
         }
     }
 }
