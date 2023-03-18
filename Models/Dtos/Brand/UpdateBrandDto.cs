e16c13d207a7e7819f63ff90b53a7b8ad003ecd2
NhanNHHCE140059 <67252133+NhanNHHCE140059@users.noreply.github.com>
2023-03-18 14:59:35 +0700

    Update UpdateBrandDto.cs

--- Models/Dtos/Brand/UpdateBrandDto.cs
+++ Models/Dtos/Brand/UpdateBrandDto.cs
@@ -1,12 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
 namespace SWP391.Project.Models.Dtos.Brand
 {
-    public class UpdateBrandDto
-    {
-        
-    }
+    public class UpdateBrandDto { }
 }
